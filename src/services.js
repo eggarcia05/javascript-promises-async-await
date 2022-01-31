2bbb551cf37400e35619882bace1763c0bc343f6
eggarcia05 <erick.14.05.1998@gmail.com>
2022-01-31 18:15:57 -0500

    promise race and promise all

--- src/services.js
+++ src/services.js
@@ -1,3 +1,17 @@
-export const fetchWithTimeout = (delay) =>{
-    return new Promise(resolve => setTimeout(resolve, delay));
-}
+export const fetchWithTimeout = (delay) => {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+};
+
+export const fetchMovies = () => {
+  return fetch("./data/movies.json")
+    .then((response) => response.json())
+    .then((movies) => movies)
+    .catch((error) => console.log(error));
+};
+
+export const fetchBooks = () => {
+  return fetch("./data/books.json")
+    .then((response) => response.json())
+    .then((books) => books)
+    .catch((error) => console.log(error));
+};
